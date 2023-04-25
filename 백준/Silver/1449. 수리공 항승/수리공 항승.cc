f1970bf4b02110b7292a6dcdd34cf9e8e520f3b0
HeroJho <89238479+HeroJho@users.noreply.github.com>
2023-04-25 12:06:09 +0900

    [Silver III] Title: 수리공 항승, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1449. 수리공 항승/수리공 항승.cc
+++ 백준/Silver/1449. 수리공 항승/수리공 항승.cc
@@ -1,75 +1,34 @@
+#include<vector>
 #include<iostream>
-#include<list>
 #include<algorithm>
 
 using namespace std;
 
 int main()
 {
-    int RR, TL; cin >> RR; cin >> TL;
-    list<int> P;
-    for (int i = 0; i < RR; ++i)
+    int N, TL; cin >> N; cin >> TL;
+    vector<int> Rs;
+    for(int i = 0; i < N; ++i)
     {
         int temp; cin >> temp;
-        P.push_back(temp);
+        Rs.push_back(temp);
     }
-
-    if (TL == 1)
+    
+    sort(Rs.begin(), Rs.end());
+    
+    int iCount = 0;
+    int iStart = Rs[0];
+    for(int i = 1; i < N; ++i)
     {
-        cout << P.size();
-        return 0;
-    }
-
-    P.sort();
-    // sort(P.begin(), P.end());
-    // 둘의 차이를 구한다.
-    int iTCount = 0;
-    int iCurL = P.front();
-    P.pop_front();
-    bool bCut = false;
-    while (!P.empty())
-    {
-        bCut = false;
-
-        if (-1 == iCurL)
+        if(TL <= Rs[i]-iStart)
         {
-            iCurL = P.front();
-            P.pop_front();
-
-            if (P.empty())
-            {
-                ++iTCount;
-                bCut = true;
-                break;
-            }
-        }
-
-        int iNexL = P.front();
-        P.pop_front();
-        int iD = (iNexL - iCurL) + 1;
-        if (TL < iD)
-        {
-            iCurL = iNexL;
-            ++iTCount;
-
-            if (P.empty())
-            {
-                ++iTCount;
-                bCut = true;
-                break;
-            }
-        }
-        else if (iD == TL)
-        {
-            iCurL = -1;
-            ++iTCount;
-            bCut = true;
+            ++iCount;
+            iStart = Rs[i];
         }
     }
-
-    if (!bCut)
-        ++iTCount;
-    cout << iTCount;
-
+    
+    ++iCount;
+    cout << iCount;
+    
     return 0;
 }
