6c1609159d9c7f967a4b5e644d00969b57841e59
HeroJho <89238479+HeroJho@users.noreply.github.com>
2023-05-09 17:25:24 +0900

    [Silver IV] Title: 수 찾기, Time: 52 ms, Memory: 3688 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -5,39 +5,67 @@
 
 using namespace std;
 
+int g_iN, g_iM;
+vector<int> g_iNs;
+vector<int> g_iMs;
+
+bool isIn(int iValue)
+{
+	int iLeft = 0;
+	int iRight = g_iNs.size() - 1;
+	int iMid = 0;
+	while (iLeft <= iRight)
+	{
+		iMid = (iLeft + iRight) / 2;
+
+		if (g_iNs[iMid] == iValue)
+			return true;
+		else if (g_iNs[iMid] < iValue)
+		{
+			iLeft = iMid + 1;
+		}
+		else if (g_iNs[iMid] > iValue)
+		{
+			iRight = iMid - 1;
+		}
+	}
+
+	return false;
+}
+
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 
-	int iN;
-	cin >> iN;
-	map<int, int> iMapNums;
-	for (int i = 0; i < iN; ++i)
+	cin >> g_iN;
+	for (int i = 0; i < g_iN; ++i)
 	{
 		int iTemp; cin >> iTemp;
-		iMapNums.insert({ iTemp, iTemp });
+		g_iNs.push_back(iTemp);
 	}
-	int iM;
-	cin >> iM;
-	vector<int> iNums;
-	for (int i = 0; i < iM; ++i)
+	cin >> g_iM;
+	for (int i = 0; i < g_iM; ++i)
 	{
 		int iTemp; cin >> iTemp;
-		iNums.push_back(iTemp);
+		g_iMs.push_back(iTemp);
 	}
 
-	for (int i = 0; i < iM; ++i)
+	
+	sort(g_iNs.begin(), g_iNs.end());
+
+	for (int i = 0; i < g_iM; ++i)
 	{
-		if (iMapNums.find(iNums[i]) != iMapNums.end())
+		if (isIn(g_iMs[i]))
 		{
-			cout << 1 << '\n';
+			cout << '1' << '\n';
 		}
 		else
 		{
-			cout << 0 << '\n';
+			cout << '0' << '\n';
 		}
 	}
+
 
 	return 0;
 }
