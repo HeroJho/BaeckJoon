8f2ffaca0bda8f4f54515bd660178be37d403b14
HeroJho <89238479+HeroJho@users.noreply.github.com>
2023-06-15 14:16:50 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <map>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+
+	int iT;
+	cin >> iT;
+	vector<int> iAnss;
+	for (int t = 0; t < iT; ++t)
+	{
+		int iN;
+		cin >> iN;
+		map<string, int> Inputs;
+		for (int i = 0; i < iN; ++i)
+		{
+			string sTemp1, sTemp2;
+			cin >> sTemp1 >> sTemp2;
+			++Inputs[sTemp2];
+		}
+
+		int iAns = 1;
+
+		for (auto pair : Inputs)
+		{
+			iAns *= pair.second + 1;
+		}
+
+		iAnss.push_back(iAns);
+	}
+
+	for (auto iAns : iAnss)
+	{
+		cout << iAns - 1 << '\n';
+	}
+
+	return 0;
+}
