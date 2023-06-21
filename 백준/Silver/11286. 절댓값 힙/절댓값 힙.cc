0a8422e7a20211208d303d2ea6f4a1fd19fb7ca9
HeroJho <89238479+HeroJho@users.noreply.github.com>
2023-06-21 22:41:56 +0900

    [Silver I] Title: 절댓값 힙, Time: 16 ms, Memory: 2656 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.cc
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+class Fuc
+{
+public:
+	bool operator()(int iL, int iR)
+	{
+		if (abs(iL) == abs(iR))
+		{
+			return iL > iR;
+		}
+		else
+		{
+			return abs(iL) > abs(iR);
+		}
+	}
+};
+
+int main()
+{
+	ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+	int iN;
+	cin >> iN;
+	vector<int> iAnss;
+	priority_queue<int, vector<int>, Fuc> iQs;
+	for (int i = 0; i < iN; ++i)
+	{
+		int iTemp;
+		cin >> iTemp;
+		if (iTemp)
+		{
+			iQs.push(iTemp);
+		}
+		else
+		{
+			if (iQs.empty())
+			{
+				iAnss.push_back(0);
+			}
+			else
+			{
+				iAnss.push_back(iQs.top());
+				iQs.pop();
+			}
+		}
+	}
+
+	for (auto iAns : iAnss)
+	{
+		cout << iAns << '\n';
+	}
+	
+
+	return 0;
+}
