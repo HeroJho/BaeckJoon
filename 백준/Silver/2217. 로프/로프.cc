3f33a12d8355708f3f5d5aa64423cd97047a2579
HeroJho <89238479+HeroJho@users.noreply.github.com>
2023-07-07 16:21:17 +0900

    [Silver IV] Title: 로프, Time: 12 ms, Memory: 2920 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.cc
+++ 백준/Silver/2217. 로프/로프.cc
@@ -1,38 +1,40 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
+#include <iostream>
+#include <vector>
+#include <list>
+#include <string>
+#include <algorithm>
+
 using namespace std;
 
 int main()
 {
-    int N = 0;
-    cin >> N;
-    vector<int> As;
-    for(int i = 0; i < N; ++i)
-    {
-        int iTemp = 0;
-        cin >> iTemp;
-        As.push_back(iTemp);
-    }
-    
-    // L = W/N    L * N = W;
-    //  10 = 20/2
-    
-    // 제일 쌘 줄 부터 달아서 최소 무게를 정한다.
-    // 그다음 내림차순으로 줄을 달아보면서 최대 무게를 갱신한다.
-    sort(As.rbegin(), As.rend());
-    
-    int iMaxW = As.front();
-    for(int i = 1; i < N; ++i)
-    {
-        int iN = i + 1;
-        int iL = As[i];
-        
-        if(iMaxW < iL * iN)
-            iMaxW = iL * iN;
-    }
-    
-    cout << iMaxW << endl;
-    
-    return 0;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int iN;
+	cin >> iN;
+	vector<int> iNums;
+	for (int i = 0; i < iN; ++i)
+	{
+		int iTemp;
+		cin >> iTemp;
+		iNums.push_back(iTemp);
+	}
+
+	sort(iNums.rbegin(), iNums.rend());
+
+	int iMax = iNums[0];
+	for (int i = 1; i < iN; ++i)
+	{
+		int iRopeCount = i + 1;
+		int iWeight = iNums[i] * iRopeCount;
+		if (iWeight > iMax)
+		{
+			iMax = iWeight;
+		}
+	}
+
+	cout << iMax;
+
+	return 0;
 }
