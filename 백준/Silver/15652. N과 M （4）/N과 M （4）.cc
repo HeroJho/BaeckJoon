c8055c2d4ff2596c54f83aefaf4911e5fd9ac2c1
HeroJho <89238479+HeroJho@users.noreply.github.com>
2023-08-29 10:48:39 +0900

    [Silver III] Title: N과 M (4), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int g_iN, g_iM;
+bool g_Check[9] = { false };
+vector<int> g_Anss;
+
+void DFS(int iDepth, int iNum)
+{
+	if (iDepth == g_iM)
+	{
+		for (int i = 0; i < g_Anss.size(); ++i)
+		{
+			cout << g_Anss[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+
+	for (int i = 1; i <= g_iN; ++i)
+	{
+		//if (g_Check[i])
+		//	continue;
+
+		if (iNum > i)
+			continue;
+
+		g_Check[i] = true;
+		g_Anss.push_back(i);
+
+		DFS(iDepth + 1, i);
+		
+		g_Check[i] = false;
+		g_Anss.pop_back();
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> g_iN >> g_iM;
+
+	DFS(0, 0);
+
+
+	return 0;
+}
