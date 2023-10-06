8147441c1c19cc57734f966e6cfbf24c11fef6f9
HeroJho <89238479+HeroJho@users.noreply.github.com>
2023-10-06 14:43:20 +0900

    [Silver I] Title: 회의실 배정, Time: 84 ms, Memory: 5108 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.cc
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.cc
@@ -1,73 +1,60 @@
-#include <iostream>
-#include <vector>
-#include <list>
-#include <string>
-#include <algorithm>
-#include <queue>
-
+#include<iostream>
+#include<algorithm>
+#include<vector>
 using namespace std;
 
-struct TimeData
+struct Time
 {
-	int iStart = 0;
-	int iEnd = 0;
-};
-
-class Fuc
-{
-public:
-	bool operator()(TimeData L, TimeData R)
-	{
-		return L.iEnd < R.iEnd;
-	}
-};
-
-class Fuc1
-{
-public:
-	bool operator()(TimeData L, TimeData R)
-	{
-		return L.iStart < R.iStart;
-	}
+    int iStart;
+    int iEnd;
 };
 
 int main()
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
+    int N = 0; cin >> N;
 
-	int iN;
-	cin >> iN;
-	
-	vector<TimeData> TimeDatas;
-	for (int i = 0; i < iN; ++i)
-	{
-		TimeData Data;
-		cin >> Data.iStart;
-		cin >> Data.iEnd;
-		TimeDatas.push_back(Data);
-	}
+    vector<Time> Times;
+    for (int i = 0; i < N; ++i)
+    {
+        int iStart, iEnd; cin >> iStart; cin >> iEnd;
+        Time Temp = { iStart, iEnd };
+        Times.push_back(Temp);
+    }
 
-	sort(TimeDatas.begin(), TimeDatas.end(), Fuc1());
-	stable_sort(TimeDatas.begin(), TimeDatas.end(), Fuc());
+    // 시작 시간으로 정렬한다
+    sort(Times.begin(), Times.end(), [](Time ValueA, Time ValueB)
+        {
+            return ValueA.iStart < ValueB.iStart;
+        });
 
-	int iCount = 1;
-	int iMaxEnd = 0;
-	TimeData Cur = TimeDatas[0];
-	iMaxEnd = Cur.iEnd;
+    // 앞 시간부터 하나씩 받는다.
+    vector<Time> TempTimes;
+    TempTimes.push_back(Times.front());
+    for (int i = 1; i < N; ++i)
+    {
+        // 가장 최근 예약된 회의
+        Time TempTime = TempTimes.back();
 
+        if (Times[i].iStart == Times[i].iEnd && Times[i].iStart == TempTime.iStart)
+        {
+            TempTimes.push_back(Times[i]);
+        }
+        // 최근꺼랑 시간이 완전 겹치고 더 작다 -> 변경
+        else if (TempTime.iEnd > Times[i].iEnd)
+        {
+            TempTimes.pop_back();
+            TempTimes.push_back(Times[i]);
+        }
+        // 최근꺼랑 시작 시간이 겹치고 끝나는 시간이 더 커서 안 겹치면 어차피 버려야한다 
 
-	for (int i = 1; i < TimeDatas.size(); ++i)
-	{
-		Cur = TimeDatas[i];
-		if (iMaxEnd <= Cur.iStart)
-		{
-			++iCount;
-			iMaxEnd = Cur.iEnd;
-		}
-	}
+        // 둘 다 안 겹쳤을 때 추가로 넣는다
+        else if (TempTime.iEnd <= Times[i].iStart)
+        {
+            TempTimes.push_back(Times[i]);
+        }
+    }
 
-	cout << iCount;
+    cout << TempTimes.size() << endl;
 
-	return 0;
+    return 0;
 }
