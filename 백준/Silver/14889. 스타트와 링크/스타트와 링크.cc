f2e9552569aada1253782636665a12b4ebf37cc6
HeroJho <89238479+HeroJho@users.noreply.github.com>
2023-11-18 02:21:07 +0900

    [Silver I] Title: 스타트와 링크, Time: 104 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.cc
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.cc
@@ -2,98 +2,64 @@
 #include <vector>
 #include <list>
 #include <string>
+#include <queue>
 #include <algorithm>
 #include "limits.h"
+#include <math.h>
 
 using namespace std;
 
 int g_iN;
 int g_Matrix[20][20] = { 0 };
-bool g_Visited[20] = { false };
-vector<int> g_Teams[2];
-
+bool g_Ts[20] = { false };
 int g_iAns = INT_MAX;
 
-
-void Cul()
+void DFS(int iDepth, int iPreIndex)
 {
-	int iOneSum = 0;
-	int iTwoSum = 0;
-	for (int i = 0; i < g_Teams[0].size(); ++i)
+	if (iDepth == g_iN / 2)
 	{
-		for (int j = 0; j < g_Teams[0].size(); ++j)
+		vector<int> L, R;
+		for (int i = 0; i < g_iN; ++i)
 		{
-			if (i == j)
-				continue;
-			iOneSum += g_Matrix[g_Teams[0][i]][g_Teams[0][j]];
-			iTwoSum += g_Matrix[g_Teams[1][i]][g_Teams[1][j]];
+			if (g_Ts[i])
+				L.push_back(i);
+			else
+				R.push_back(i);
+
 		}
-	}
-	
-	int iDis = abs(iOneSum - iTwoSum);
-	if (g_iAns > iDis)
-		g_iAns = iDis;
 
-	//for (int i = 0; i < g_Teams[0].size(); ++i)
-	//{
-	//	cout << g_Teams[0][i] << ' ';
-	//}
-	//cout << endl;
-	//for (int i = 0; i < g_Teams[1].size(); ++i)
-	//{
-	//	cout << g_Teams[1][i] << ' ';
-	//}
-	//cout << endl;
-	//cout << endl;
+		int iLSum = 0;
+		for (int i = 0; i < L.size(); ++i)
+		{
+			for (int j = i + 1; j < L.size(); ++j)
+			{
+				iLSum += g_Matrix[L[i]][L[j]];
+				iLSum += g_Matrix[L[j]][L[i]];
+			}
+		}
 
-}
+		int iRSum = 0;
+		for (int i = 0; i < R.size(); ++i)
+		{
+			for (int j = i + 1; j < R.size(); ++j)
+			{
+				iRSum += g_Matrix[R[i]][R[j]];
+				iRSum += g_Matrix[R[j]][R[i]];
+			}
+		}
 
-void DFS(int iLNum, int iRNum, int iDepth)
-{
-	if (iDepth == g_iN)
-	{
-		Cul();
+		int iDis = abs(iRSum - iLSum);
+		if (g_iAns > iDis)
+			g_iAns = iDis;
+
 		return;
 	}
 
-	int iStart = 0;
-	if (iDepth < g_iN / 2)
-		iStart = iLNum;
-	else
-		iStart = iRNum;
-
-	for (int i = iStart; i < g_iN; ++i)
+	for (int i = iPreIndex + 1; i < g_iN; ++i)
 	{
-
-		if (g_Visited[i])
-			continue;
-
-		if (iDepth < g_iN / 2)
-		{
-			if (iDepth == 0)
-			{
-				if(i != 0)
-					continue;
-			}
-			
-
-			g_Teams[0].push_back(i);
-			g_Visited[i] = true;
-			DFS(i, 0, iDepth + 1);
-			g_Teams[0].pop_back();
-			g_Visited[i] = false;
-		}
-		else
-		{
-
-
-			g_Teams[1].push_back(i);
-			g_Visited[i] = true;
-			DFS(0, i, iDepth + 1);
-			g_Teams[1].pop_back();
-			g_Visited[i] = false;
-		}
-
+		g_Ts[i] = true;
+		DFS(iDepth + 1, i);
+		g_Ts[i] = false;
 	}
 
 }
@@ -104,17 +70,17 @@ int main()
 	cin.tie(NULL);
 
 	cin >> g_iN;
-	for (int y = 0; y < g_iN; ++y)
+	for (int i = 0; i < g_iN; ++i)
 	{
-		for (int x = 0; x < g_iN; ++x)
+		for (int j = 0; j < g_iN; ++j)
 		{
-			cin >> g_Matrix[y][x];
+			cin >> g_Matrix[i][j];
 		}
 	}
 
-	DFS(0, 0, 0);
+	DFS(0, -1);
 
- 	cout << g_iAns;
+	cout << g_iAns;
 
 	return 0;
 }
