92c5a57ea0286e4589fe7776ad495033c8facfb2
HeroJho <89238479+HeroJho@users.noreply.github.com>
2024-01-01 17:48:56 +0900

    [Silver I] Title: 볼 모으기, Time: 0 ms, Memory: 2024 KB, Score: 15 point -BaekjoonHub

---
+++ 백준/Silver/17615. 볼 모으기/볼 모으기.cc
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <string>
+#include <queue>
+#include <algorithm>
+#include "limits.h"
+
+using namespace std;
+
+int Run(char Check, int N, string S)
+{
+	int Left = 0, Right = S.size() - 1;
+	
+	while (Right > 0 && S[Right] == Check)
+	{
+		// Right 찾기
+		// Left 찾기
+		--Right;
+	}
+
+	int iCount = 0;
+
+	Left = Right;
+	while (Left >= 0)
+	{
+		if (S[Left] == Check)
+		{
+			--Right;
+			++iCount;
+		}
+
+		--Left;
+	}
+
+	return iCount;
+}
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int N;
+	cin >> N;
+	string S;
+	cin >> S;
+
+	int iAns = 0;
+	iAns = Run('R', N, S);
+	iAns = min(iAns, Run('B', N, S));
+
+	cout << iAns;
+
+	return 0;
+}
