4f97b776b70df35c927585aaf40df442cdf9509f
HeroJho <89238479+HeroJho@users.noreply.github.com>
2024-01-20 10:23:08 +0900

    [Silver V] Title: 단어 정렬, Time: 60 ms, Memory: 4756 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.cc
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.cc
@@ -1,48 +1,57 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 #include <algorithm>
-#include <string>
+#include <set>
+#include "limits.h"
 
 using namespace std;
 
 class Func1
 {
 public:
-	bool operator()(string sL, string sR)
+	bool operator()(string L, string R)
 	{
-		return sL.size() < sR.size();
+		return L.length() < R.length();
 	}
 };
 
 class Func2
 {
 public:
-	bool operator()(string sL, string sR)
+	bool operator()(string L, string R)
 	{
-		return sL.size() == sR.size() && sL < sR;
+		return L.length() == R.length() && L < R;
 	}
 };
 
+int g_N;
+vector<string> g_Inputs;
+
 int main()
 {
-	int iN = 0;
-	cin >> iN;
-	vector<string> sSs;
-	for (int i = 0; i < iN; ++i)
-	{
-		string sTemp;
-		cin >> sTemp;
-		sSs.push_back(sTemp);
-	}
-	
-	sort(sSs.begin(), sSs.end(), Func1());
-	stable_sort(sSs.begin(), sSs.end(), Func2());
-	sSs.erase(unique(sSs.begin(), sSs.end()), sSs.end());
+	ios::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
 
-	for (int i = 0; i < sSs.size(); ++i)
+	cin >> g_N;
+	for (int i = 0; i < g_N; ++i)
 	{
-		cout << sSs[i] << endl;
+		string Temp;
+		cin >> Temp;
+		g_Inputs.push_back(Temp);
 	}
+
+	sort(g_Inputs.begin(), g_Inputs.end(), Func1());
+	stable_sort(g_Inputs.begin(), g_Inputs.end(), Func2());
+
+	auto iter = unique(g_Inputs.begin(), g_Inputs.end());
+	g_Inputs.erase(iter, g_Inputs.end());
+
+	for (int i = 0; i < g_Inputs.size(); ++i)
+	{
+		cout << g_Inputs[i] << '\n';
+	}
+
 
 	return 0;
 }
