8cf3ca1cc04f9c9b14ba7b6acf998a2973b24b27
HeroJho <89238479+HeroJho@users.noreply.github.com>
2024-01-22 08:10:54 +0900

    [Gold III] Title: 내리막 길, Time: 28 ms, Memory: 41084 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.cc
+++ 백준/Gold/1520. 내리막 길/내리막 길.cc
@@ -1,71 +1,84 @@
 #include <iostream>
-
-#define MAX 500
+#include <vector>
+#include <queue>
 
 using namespace std;
 
-int g_iN, g_iM;
-int g_Matrix[MAX][MAX] = { 0 };
-int g_DP[MAX][MAX] = { 0 };
+
+int g_N, g_M;
+int g_Matrix[500][10000] = { 0 };
+int g_DP[500][10000] = { 0 };
 
 int g_DirX[] = { 0, 0, -1, 1 };
-int g_DirY[] = { 1, -1, 0, 0 };
+int g_DirY[] = { -1, 1, 0, 0 };
 
 void Reset()
 {
-	for (int y = 0; y < g_iN; ++y)
-	{
-		for (int x = 0; x < g_iM; ++x)
-		{
-			g_DP[y][x] = -1;
-		}
-	}
+    for (int y = 0; y < g_M; ++y)
+    {
+        for (int x = 0; x < g_N; ++x)
+        {
+            g_DP[y][x] = -1;
+        }
+    }
 }
 
-bool IsIn(int iX, int iY)
+bool IsIn(int X, int Y)
 {
-	return iX >= 0 && iX < g_iM&& iY >= 0 && iY < g_iN;
+    return X >= 0 && X < g_N && Y >= 0 && Y < g_M;
 }
 
-int DFS(int iX, int iY)
+
+int DFS(pair<int ,int> Pos)
 {
-	if (iX == g_iM - 1 && iY == g_iN - 1)
-		return 1;
-	else if (g_DP[iY][iX] != -1)
-		return g_DP[iY][iX];
+    if(Pos.first == g_N - 1 && Pos.second == g_M - 1)
+    {
+        return 1;
+    }
+    else if (g_DP[Pos.second][Pos.first] != - 1)
+    {
+        return g_DP[Pos.second][Pos.first];
+    }
 
-	g_DP[iY][iX] = 0;
-	for (int i = 0; i < 4; ++i)
-	{
-		int inX = iX + g_DirX[i];
-		int inY = iY + g_DirY[i];
+    g_DP[Pos.second][Pos.first] = 0;
 
-		if (IsIn(inX, inY) && g_Matrix[inY][inX] < g_Matrix[iY][iX])
-		{
-			g_DP[iY][iX] = g_DP[iY][iX] + DFS(inX, inY);
-		}
+    for (int i = 0; i < 4; ++i)
+    {
+        int inX = g_DirX[i] + Pos.first;
+        int inY = g_DirY[i] + Pos.second;
 
-	}
+        if (!IsIn(inX, inY))
+            continue;
 
-	return g_DP[iY][iX];
+        if (g_Matrix[inY][inX] >= g_Matrix[Pos.second][Pos.first])
+            continue;
+        
+        g_DP[Pos.second][Pos.first] += DFS({inX, inY});
+
+    }
+    
+    return g_DP[Pos.second][Pos.first];
 }
+
 
 int main()
 {
-	cin >> g_iN >> g_iM;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-	for (int y = 0; y < g_iN; ++y)
-	{
-		for (int x = 0; x < g_iM; ++x)
-		{
-			cin >> g_Matrix[y][x];
-		}
-	}
+    cin >> g_M >> g_N;
+    for (int y = 0; y < g_M; ++y)
+    {
+        for (int x = 0; x < g_N; ++x)
+        {
+            cin >> g_Matrix[y][x];
+        }
+    }
 
-	Reset();
+    Reset();
+    DFS({0, 0});
 
-	DFS(0, 0);
-	cout << g_DP[0][0];
+    cout << g_DP[0][0];
 
-	return 0;
+    return 0;
 }
