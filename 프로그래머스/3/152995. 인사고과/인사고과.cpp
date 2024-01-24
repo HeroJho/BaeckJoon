28c958ee1a91cc57c3e8f0543f6aaaac1ee875c4
HeroJho <89238479+HeroJho@users.noreply.github.com>
2024-01-24 19:26:38 +0900

    [level 3] Title: 인사고과, Time: 343.58 ms, Memory: 23.8 MB -BaekjoonHub

--- 프로그래머스/3/152995. 인사고과/인사고과.cpp
+++ 프로그래머스/3/152995. 인사고과/인사고과.cpp
@@ -1,127 +1,112 @@
 #include <iostream>
 #include <vector>
-#include <list>
-#include <string>
 #include <queue>
 #include <algorithm>
+#include <set>
 #include "limits.h"
 
 using namespace std;
 
-struct Data
-{
-	pair<int, int> Score;
-	int iIndex = 0;
-
-	Data() {};
-	Data(pair<int, int> _Score, int _iIndex) : Score(_Score), iIndex(_iIndex) {};
-};
+bool g_Nos[100001] = { false };
 
 class Func
 {
 public:
-	bool operator() (Data L, Data R)
+	bool operator()(vector<int> L, vector<int> R)
 	{
-		return L.Score.first > R.Score.first;
-	}
-};
-class Func3
-{
-public:
-	bool operator() (Data L, Data R)
-	{
-		return L.Score.first == R.Score.first && L.Score.second < R.Score.second;
-	}
-};
-class Func1
-{
-public:
-	bool operator() (pair<int ,int> L, pair<int, int> R)
-	{
-		return L.first > R.first;
+		return L[0] > R[0];
 	}
 };
 
+class Func2
+{
+public:
+	bool operator()(vector<int> L, vector<int> R)
+	{
+		return L[0] == R[0] && L[1] < R[1];
+	}
+};
+
+class Func3
+{
+public:
+	bool operator()(vector<int> L, vector<int> R)
+	{
+		return L[3] > R[3];
+	}
+};
 
 int solution(vector<vector<int>> scores) {
 	int answer = 0;
 
-	vector<Data> Inputs;
+	// 2 = 인덱스
 	for (int i = 0; i < scores.size(); ++i)
 	{
-		Data Temp;
-		Temp.Score.first = scores[i][0];
-		Temp.Score.second = scores[i][1];
-		Temp.iIndex = i;
-		Inputs.push_back(Temp);
+		scores[i].push_back(i);
 	}
 
-	sort(Inputs.begin(), Inputs.end(), Func());
-	stable_sort(Inputs.begin(), Inputs.end(), Func3());
+	// 0 내림 1 오름
+	sort(scores.begin(), scores.end(), Func());
+	stable_sort(scores.begin(), scores.end(), Func2());
 
-	vector<Data> Temps;
-	Temps.push_back(Inputs[0]);
-	int iMax = Inputs[0].Score.second;
-	for (int i = 1; i < Inputs.size(); ++i)
+
+	int Min = scores[0][1];
+	for (int i = 0; i < scores.size() - 1; ++i)
 	{
-		if (Inputs[0].Score.first == Inputs[i].Score.first)
+		if (Min > scores[i][1])
 		{
-			Temps.push_back(Inputs[i]);
-			if (iMax <= Inputs[i].Score.second)
-			{
-				iMax = Inputs[i].Score.second;
-			}
-		}
-		else
-		{
-			if (iMax <= Inputs[i].Score.second)
-			{
-				Temps.push_back(Inputs[i]);
-				iMax = Inputs[i].Score.second;
-			}
+			g_Nos[i] = true;
 		}
 
-
-	}
-
-	vector<pair<int, int>> Temps2;
-	for (int i = 0; i < Temps.size(); ++i)
-	{
-		Temps2.push_back({ Temps[i].Score.first + Temps[i].Score.second, Temps[i].iIndex});
-	}
-
-	sort(Temps2.begin(), Temps2.end(), Func1());
-
-
-	int iCount = 0;
-	int iHoldScore = Temps2[0].first;
-	if (Temps2[0].second == 0)
-	{
-		return iCount + 1;
-	}
-
-
-	for (int i = 1; i < Temps2.size(); ++i)
-	{
-		if (iHoldScore == Temps2[i].first)
+		if (scores[i][0] != scores[i + 1][0])
 		{
-			if (Temps2[i].second == 0)
-			{
-				return iCount + 1;
-			}
-		}
-		else
-		{
-			iHoldScore = Temps2[i].first;
-			iCount = i;
-			if (Temps2[i].second == 0)
-			{
-				return i + 1;
-			}
+			if(Min < scores[i][1])
+				Min = scores[i][1];
 		}
 
 	}
 
+	if (Min > scores[scores.size() - 1][1])
+	{
+		g_Nos[scores.size()-1] = true;
+	}
+	
+
+
+	int Count = -1;
+	for (auto iter = scores.begin(); iter != scores.end();)
+	{
+		++Count;
+		if (g_Nos[Count])	
+		{
+			iter = scores.erase(iter);
+			continue;
+		}
+
+		int Score = (*iter)[0] + (*iter)[1];
+		iter->push_back(Score);
+
+		++iter;
+	}
+
+	sort(scores.begin(), scores.end(), Func3());
+
+
+	int Rank = 0;
+	int PreScore = -1;
+	for (int i = 0; i < scores.size(); ++i)
+	{
+		if (PreScore != scores[i][3])
+		{
+			Rank = i + 1;
+			PreScore = scores[i][3];
+		}
+
+		if (scores[i][2] == 0)
+		{
+			return Rank;
+		}
+	}
 
 	return -1;
 }
