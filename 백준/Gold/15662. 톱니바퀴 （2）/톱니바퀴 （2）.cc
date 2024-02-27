f2e34c1a85f3406935ed351a69a847cd76f986bf
HeroJho <89238479+HeroJho@users.noreply.github.com>
2024-02-27 16:09:33 +0900

    [Gold V] Title: 톱니바퀴 (2), Time: 0 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Gold/15662. 톱니바퀴 （2）/톱니바퀴 （2）.cc
+++ 백준/Gold/15662. 톱니바퀴 （2）/톱니바퀴 （2）.cc
@@ -1,61 +1,117 @@
 #include <iostream>
 #include <vector>
-using namespace std;
-int T, K, tmp, dir, ret, arr[1004][8], pointer[1004];
-vector<pair<int, int>> v;
+#include <queue>
 
-void go() {
-	for (pair<int, int> p : v) {
-		int check[1004] = { 0 };
-		tmp = p.first;
-		dir = p.second;
-		check[tmp] = dir;
-		while (1) {
-			if (tmp <= 1) break;
-			int c1 = (pointer[tmp] + 6) % 8;
-			int c2 = (pointer[tmp - 1] + 2) % 8;
-			if (arr[tmp][c1] == arr[tmp - 1][c2]) break;
-			else {
-				dir *= -1;
-				check[tmp - 1] = dir;
-			}
-			tmp--;
-		}
-		while (1) {
-			if (tmp >= T) break;
-			int c1 = (pointer[tmp] + 2) % 8;
-			int c2 = (pointer[tmp + 1] + 6) % 8;
-			if (arr[tmp][c1] == arr[tmp + 1][c2]) break;
-			else {
-				dir *= -1;
-				check[tmp + 1] = dir;
-			}
-			tmp++;
-		}
-		for (int i = 1; i <= T; i++) {
-			if (check[i] == -1) pointer[i] = (pointer[i] + 1) % 8;
-			else if (check[i] == 1) pointer[i] = (pointer[i] + 7) % 8;
-		}
-	}
+using namespace std;
+
+int N, M;
+vector<string> Inputs;
+vector<pair<int, int>> RollInfos;
+
+// 돌리는 함수
+void Roll(string& sS, int Dir)
+{
+    if (Dir == 1)
+    {
+        // 1001
+        // 1100  시계
+        int Last = sS.back();
+        for (int i = sS.length() - 2; i >= 0; --i)
+        {
+            sS[i + 1] = sS[i];
+        }
+        sS.front() = Last;
+    }
+    else
+    {   // 1001
+        // 0011
+        int First = sS.front();
+        for (int i = 1; i < sS.length(); ++i)
+        {
+            sS[i - 1] = sS[i];
+        }
+        sS.back() = First;
+    }
 }
 
-int main() {
-	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	cin >> T;
-	string str;
-	for (int i = 1; i <= T; i++) {
-		cin >> str;
-		for (int k = 0; k < str.size(); k++)
-			arr[i][k] = str[k] - '0';
-	}
-	cin >> K;
-	for (int i = 0; i < K; i++) {
-		cin >> tmp >> dir;
-		v.push_back({ tmp,dir });
-	}
-	go();
-	for (int i = 1; i <= T; i++) {
-		if (arr[i][pointer[i]] == 1) ret++;
-	}
-	cout << ret;
+void Go(int Index, int PreIndex, int Dir)
+{
+
+
+    // 2 6
+    // 2 = 뒤
+    // 6 = 앞
+
+    // 내 앞 뒤 인덱스 애들 돌린다
+    int Front = Index - 1;
+    if (PreIndex != Front && Front >= 0 && Front < N)
+    {
+        // 나의 앞
+        if (Inputs[Index][6] != Inputs[Front][2])
+        {
+            // 반대로 돌린다
+            Go(Front, Index, Dir * -1);
+        }
+
+    }
+
+    int Back = Index + 1;
+    if (PreIndex != Back && Back >= 0 && Back < N)
+    {
+        if (Inputs[Index][2] != Inputs[Back][6])
+        {
+            Go(Back, Index, Dir * -1);
+        }
+
+    }
+
+    // 현재 돌린다
+    Roll(Inputs[Index], Dir);
+}
+
+int CheckAns()
+{
+    int Count = 0;
+    for (int i = 0; i < N; ++i)
+    {
+        if (Inputs[i][0] == '1')
+            ++Count;
+    }
+
+    return Count;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+
+    cin >> N;
+    for (int i = 0; i < N; ++i)
+    {
+        string sTemp;
+        cin >> sTemp;
+        Inputs.push_back(sTemp);
+    }
+    cin >> M;
+    for (int i = 0; i < M; ++i)
+    {
+        pair<int, int> Temp;
+        cin >> Temp.first >> Temp.second;
+        RollInfos.push_back(Temp);
+    }
+    
+
+    for (int i = 0; i < M; ++i)
+    {
+        pair<int, int> Info = RollInfos[i];
+        Go(Info.first - 1, -1, Info.second);
+    }
+
+
+    cout << CheckAns();
+
+
+    return 0;
 }
