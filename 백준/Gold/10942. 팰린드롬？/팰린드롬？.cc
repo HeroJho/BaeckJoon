6304f980020eb24f13df220e6b0bab3ee212a9e7
HeroJho <89238479+HeroJho@users.noreply.github.com>
2024-03-05 19:10:06 +0900

    [Gold IV] Title: 팰린드롬?, Time: 248 ms, Memory: 18364 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.cc
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.cc
@@ -1,38 +1,61 @@
 #include <iostream>
+#include <vector>
+#include <queue>
+
 using namespace std;
 
-int N,M;
-int arr[2010];
-int dp[2010][2010];
 
-int main(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
-    
-    cin >> N ;
-    for(int i=1; i<=N; i++) cin >> arr[i];
-    cin >> M;
-    
-    for(int i=1; i<=N; i++){
-        dp[i][i]=1; //한자리 수는 팰린드롬
-        if(i!=1 && arr[i-1] == arr[i]) dp[i-1][i]=1; //두자리수가 같은 경우 팰린드롬
+bool DP[2001][2001] = { false };
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int N;
+    cin >> N;
+    vector<int> Inputs;
+    for (int i = 0; i < N; ++i)
+    {
+        int Temp; cin >> Temp;
+        Inputs.push_back(Temp);
     }
+    int M;
+    cin >> M;
+    vector<pair<int ,int>> Orders;
+    for (int i = 0; i < M; ++i)
+    {
+        pair<int, int> Temp;
+        cin >> Temp.first >> Temp.second;
+        Orders.push_back(Temp);
+    }
+
     
-    for(int i=2; i<=N-1; i++){
-        for(int j=1; i+j<=N; j++){
-            if(arr[j]==arr[i+j] && dp[j+1][i+j-1]==1)
-                dp[j][i+j]=1;
+
+    for (int i = 0; i < N-1; ++i)
+    {
+        // 하나는 무조건 true
+        DP[i][i] = true;
+        // 둘은 두 개가 같으면 true
+        DP[i][i + 1] = Inputs[i] == Inputs[i + 1];
+    }
+    DP[N-1][N-1] = true;
+
+    // i = 간격
+    for (int i = 2; i < N; ++i)
+    {
+        for (int j = 0; j+i < N; ++j)
+        {
+            DP[j][j+i] = Inputs[j] == Inputs[j+i] && DP[j + 1][j+i - 1];
         }
     }
-    
-    while(M>0){
-        int s,e; cin >> s >> e;
-        
-        if(dp[s][e] == 1) cout << 1 << "\n";
-        else cout << 0 << "\n";
-        
-        M--;
+
+
+    for (int i = 0; i < M; ++i)
+    {
+        cout << DP[Orders[i].first - 1][Orders[i].second - 1] << '\n';
     }
-    
+
+
     return 0;
 }
