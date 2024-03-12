c02b88c5a1567d7d5f88199389751eec5b5b21e2
HeroJho <89238479+HeroJho@users.noreply.github.com>
2024-03-12 11:28:29 +0900

    [Gold V] Title: 리모컨, Time: 20 ms, Memory: 2996 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.cc
+++ 백준/Gold/1107. 리모컨/리모컨.cc
@@ -1,52 +1,67 @@
 #include <iostream>
 #include <vector>
-#include <list>
+#include <queue>
 #include <string>
-#include <algorithm>
+#include <climits>
 
 using namespace std;
 
+bool DoesMakes[1000001] = { false };
+
 int main()
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-	string sNum;
-	int iB;
-	vector<int> iBs;
-	cin >> sNum;
-	cin >> iB;
-	for (int i = 0; i < iB; ++i)
-	{
-		int iTemp; cin >> iTemp;
-		iBs.push_back(iTemp);
-	}
+    
+    int Num = 0;
+    cin >> Num;
+
+    int N;
+    cin >> N;
+    bool Brok[10] = {false};
+    for (int i = 0; i < N; ++i)
+    {
+        int Temp;
+        cin >> Temp;
+        Brok[Temp] = true;
+    }
+
+    // 제일 차가 작은 수
+    int ButtonCnt = INT_MAX;
+    for (int i = 0; i < 1000001; ++i)
+    {
+        string sNum = to_string(i);
+        bool DoesMake = true;
+        for (int s = 0; s < sNum.length(); ++s)
+        {
+            int iNum = sNum[s] - '0';
+            if (Brok[iNum])
+            {
+                DoesMake = false;
+                break;
+            }
+        }
+
+        // 만들 수 있는 숫자
+        if (DoesMake)
+        {
+            // DoesMakes[i] = true;
+            int TotalCnt = sNum.length() + abs(Num - i);
+            if (TotalCnt < ButtonCnt)
+                ButtonCnt = TotalCnt;
+        }
+    }
 
 
-	int iMinCnt = abs(100 - stoi(sNum));
-	for (int i = 0; i < 1000000; ++i)
-	{
-		string sTempNum = to_string(i);
-		bool bBr = false;
-		for (int j = 0; j < sTempNum.length(); ++j)
-		{
-			if (find(iBs.begin(), iBs.end(), sTempNum[j] - '0') != iBs.end())
-			{
-				bBr = true;
-			}
-		}
-
-		// 채널에 고장난 곳이 없다.
-		if (!bBr)
-		{
-			// 카운트 수
-			int iTempCnt = sTempNum.length() + abs(i - stoi(sNum));
-			iMinCnt = min(iMinCnt, iTempCnt);
-		}
-	}
-
-	cout << iMinCnt;
+    // 100 부터 했을 경우
+    int TotalCnt = abs(100 - Num);
+    if (TotalCnt < ButtonCnt)
+        ButtonCnt = TotalCnt;
 
 
-	return 0;
+    cout << ButtonCnt;
+    
+
+    return 0;
 }
