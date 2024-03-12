9c9dcacece8502da7ef16111535b5a4e0aa79456
HeroJho <89238479+HeroJho@users.noreply.github.com>
2024-03-12 11:30:25 +0900

    [Gold V] Title: 리모컨, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.cc
+++ 백준/Gold/1107. 리모컨/리모컨.cc
@@ -6,8 +6,6 @@
 
 using namespace std;
 
-bool DoesMakes[1000001] = { false };
-
 int main()
 {
     ios_base::sync_with_stdio(false);
@@ -27,8 +25,11 @@ int main()
         Brok[Temp] = true;
     }
 
+  
+    // 100 부터 했을 경우
+    int ButtonCnt = abs(100 - Num);
+
     // 제일 차가 작은 수
-    int ButtonCnt = INT_MAX;
     for (int i = 0; i < 1000001; ++i)
     {
         string sNum = to_string(i);
@@ -46,18 +47,11 @@ int main()
         // 만들 수 있는 숫자
         if (DoesMake)
         {
-            // DoesMakes[i] = true;
             int TotalCnt = sNum.length() + abs(Num - i);
             if (TotalCnt < ButtonCnt)
                 ButtonCnt = TotalCnt;
         }
     }
-
-
-    // 100 부터 했을 경우
-    int TotalCnt = abs(100 - Num);
-    if (TotalCnt < ButtonCnt)
-        ButtonCnt = TotalCnt;
 
 
     cout << ButtonCnt;
