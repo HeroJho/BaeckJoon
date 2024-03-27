a0e3181615df99e5ff2c0df38dc56d8571b816b3
HeroJho <89238479+HeroJho@users.noreply.github.com>
2024-03-27 16:06:29 +0900

    [Gold III] Title: 줄 세우기, Time: 28 ms, Memory: 4148 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.cc
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int N, M;
+vector<int> Matrix[32002];
+int InNode[32002] = { 0 };
+
+vector<int> Ans;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> N >> M;
+    for (int i = 0; i < M; ++i)
+    {
+        int Start, End;
+        cin >> Start >> End;
+        Matrix[Start].push_back(End);
+        ++InNode[End];
+    }
+
+    queue<int> Qs;
+    // 진입차수가 0인 정점을 모두 넣는다
+    for (int i = 1; i <= N; ++i)
+    {
+        if (InNode[i] == 0)
+            Qs.push(i);
+    }
+
+    while (!Qs.empty())
+    {
+        int Cur = Qs.front();
+        Qs.pop();
+
+        Ans.push_back(Cur);
+
+        for (int i = 0; i < Matrix[Cur].size(); ++i)
+        {
+            int Next = Matrix[Cur][i];
+            --InNode[Next];
+            if (InNode[Next] == 0)
+                Qs.push(Next);
+        }
+
+    }
+
+    for (int i = 0; i < Ans.size(); ++i)
+    {
+        cout << Ans[i] << ' ';
+    }
+
+    return 0;
+}
