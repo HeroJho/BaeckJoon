28f657c41454e25e1b562fdd3e452517a7ddc246
HeroJho <89238479+HeroJho@users.noreply.github.com>
2024-03-28 20:25:35 +0900

    [Silver III] Title: 이친수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.cc
+++ 백준/Silver/2193. 이친수/이친수.cc
@@ -4,37 +4,26 @@
 
 using namespace std;
 
-int N;
 long long DP[91][2] = { 0 };
-
-long long DFS(int Depth, int CurNum)
-{
-    if (Depth >= N)
-        return 1;
-
-    if (DP[Depth][CurNum] != 0)
-        return DP[Depth][CurNum];
-
-    // 0
-    DP[Depth][CurNum] += DFS(Depth + 1, 0);
-    // 1
-    if(CurNum == 0)
-        DP[Depth][CurNum] += DFS(Depth + 1, 1);
-
-    return DP[Depth][CurNum];
-}
-
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
+    int N;
     cin >> N;
-    
-    cout << DFS(1, 1);
 
-    // cout << DP[0][1];
+    DP[1][0] = 0;
+    DP[1][1] = 1;
+
+    for (int i = 2; i <= N; ++i)
+    {
+        DP[i][0] = DP[i - 1][0] + DP[i - 1][1];
+        DP[i][1] = DP[i - 1][0];
+    }
+
+    cout << DP[N][0] + DP[N][1];
 
     return 0;
 }
