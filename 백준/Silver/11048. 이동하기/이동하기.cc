de39c58a236aa9f6abbddf36c9865cd40df0c5ef
HeroJho <89238479+HeroJho@users.noreply.github.com>
2024-03-31 00:34:06 +0900

    [Silver II] Title: 이동하기, Time: 112 ms, Memory: 9964 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.cc
+++ 백준/Silver/11048. 이동하기/이동하기.cc
@@ -1,52 +1,72 @@
-#include<iostream>
-#include<queue>
-#include<tuple>
+#include <iostream>
+#include <vector>
+#include <queue>
+
 using namespace std;
 
-int map[1001][1001];
-bool check[1001][1001];
-int answer[1001][1001];
-int dx[3] = { 1,0,1 };
-int dy[3] = { 0,1,1 };
+int N, M;
+int Matrix[1000][1000] = { 0 };
+int DP[1000][1000] = { 0 };
+
+int DirX[] = { 0, 1, 1 };
+int DirY[] = { 1, 0, 1 };
+
+bool IsIn(int X, int Y)
+{
+    return X >= 0 && X < M && Y >= 0 && Y < N;
+}
+
+void BFS()
+{
+    queue<pair<int, int>> Qs;
+    Qs.push({0,0});
+    DP[0][0] = Matrix[0][0];
+
+    while (!Qs.empty())
+    {
+        pair<int, int> Cur = Qs.front();
+        Qs.pop();
+
+        for (int i = 0; i < 3; ++i)
+        {
+            int nX = DirX[i] + Cur.first;
+            int nY = DirY[i] + Cur.second;
+            int nV = Matrix[nY][nX] + DP[Cur.second][Cur.first];
+
+            if (!IsIn(nX, nY))
+                continue;
+
+            if (DP[nY][nX] >= nV)
+                continue;
+            DP[nY][nX] = nV;
+            
+            Qs.push({nX,nY});
+
+        }
+
+    }
+
+}
+
 int main()
 {
-	int n, m; cin >> n >> m;
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < m; j++)
-		{
-			cin >> map[i][j];
-		}
-	}
-	queue<pair<int, int>>q;
-	q.push(make_pair(0, 0));
-	check[0][0] = true;
-	answer[0][0] = map[0][0];
-	while (!q.empty())
-	{
-		int x, y; tie(x, y) = q.front();
-		q.pop();
-		for (int i = 0; i < 3; i++)
-		{
-			int nx = x + dx[i];
-			int ny = y + dy[i];
-			if (nx >= 0 && ny >= 0 && nx < m && ny < n)
-			{
-				if (check[ny][nx] == false)
-				{
-					q.push(make_pair(nx, ny));
-					check[ny][nx] = true;
-					answer[ny][nx] = answer[y][x] + map[ny][nx];
-				}
-				else if (answer[ny][nx] < answer[y][x] + map[ny][nx])
-				{
-					q.push(make_pair(nx, ny));
-					answer[ny][nx] = answer[y][x] + map[ny][nx];
-				}
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-			}
-		}
-	}
-	cout << answer[n - 1][m - 1];
-	return 0;
+    cin >> N >> M;
+    for (int i = 0; i < N; ++i)
+    {
+        for (int j = 0; j < M; ++j)
+        {
+            cin >> Matrix[i][j];
+
+            DP[i][j] = -1;
+        }
+    }
+
+    BFS();
+
+    cout << DP[N - 1][M - 1];
+
+    return 0;
 }
