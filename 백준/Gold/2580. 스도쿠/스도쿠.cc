90056a5b5b440651f2087892915ac98dfc9301a0
HeroJho <89238479+HeroJho@users.noreply.github.com>
2024-07-25 15:46:38 +0900

    [Gold IV] Title: 스도쿠, Time: 224 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/2580. 스도쿠/스도쿠.cc
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+
+using namespace std;
+
+int Matrix[9][9] = { 0 };
+
+
+bool Check(int X, int Y, int Num)
+{
+    // 가로
+    for (int i = 0; i < 9; ++i)
+    {
+        if (Matrix[Y][i] == Num)
+            return false;
+    }
+
+    // 세로 검사
+    for (int i = 0; i < 9; ++i)
+    {
+        if (Matrix[i][X] == Num)
+            return false;
+    }
+
+    // 3*3
+    // 5, 5
+    int StartX = (X / 3) * 3;
+    int StartY = (Y / 3) * 3;
+
+    for (int y = StartY; y < StartY + 3; ++y)
+    {
+        for (int x = StartX; x < StartX + 3; ++x)
+        {
+            if (Matrix[y][x] == Num)
+                return false;
+        }
+    }
+
+    return true;
+}
+
+bool DFS(int Pos)
+{
+    if (Pos == 81)
+    {
+        return true;
+    }
+
+    int X = Pos % 9;
+    int Y = Pos / 9;
+    if (Matrix[Y][X] != 0)
+    {
+        return DFS(Pos + 1);
+    }
+
+    for (int i = 1; i <= 9; ++i)
+    {
+
+        if (Check(X, Y, i))
+        {
+            Matrix[Y][X] = i;
+            if (DFS(Pos + 1))
+                return true;
+            Matrix[Y][X] = 0;  // 백트래킹
+        }
+
+    }
+
+    return false;
+}
+
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+
+    for (int i = 0; i < 9; ++i)
+    {
+        for (int j = 0; j < 9; ++j)
+        {
+            cin >> Matrix[i][j];
+        }
+    }
+
+    DFS(0);
+
+    for (int i = 0; i < 9; ++i)
+    {
+        for (int j = 0; j < 9; ++j)
+        {
+            cout << Matrix[i][j] << ' ';
+        }
+        cout << endl;
+    }
+
+
+    return 0;
+}
