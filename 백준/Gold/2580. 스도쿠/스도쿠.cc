f47ec0aca93cf8decf932bd9b72f99d03c21955d
HeroJho <89238479+HeroJho@users.noreply.github.com>
2024-07-26 09:37:58 +0900

    [Gold IV] Title: 스도쿠, Time: 224 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.cc
+++ 백준/Gold/2580. 스도쿠/스도쿠.cc
@@ -4,30 +4,24 @@
 
 using namespace std;
 
-int Matrix[9][9] = { 0 };
+int Matrix[10][10] = { 0 };
 
 
 bool Check(int X, int Y, int Num)
 {
-    // 가로
+    // 가로, 세로
     for (int i = 0; i < 9; ++i)
     {
         if (Matrix[Y][i] == Num)
             return false;
-    }
 
-    // 세로 검사
-    for (int i = 0; i < 9; ++i)
-    {
         if (Matrix[i][X] == Num)
             return false;
     }
 
-    // 3*3
-    // 5, 5
-    int StartX = (X / 3) * 3;
-    int StartY = (Y / 3) * 3;
-
+    // 3 * 3
+    int StartX = 3 * (X / 3);
+    int StartY = 3 * (Y / 3);
     for (int y = StartY; y < StartY + 3; ++y)
     {
         for (int x = StartX; x < StartX + 3; ++x)
@@ -40,36 +34,35 @@ bool Check(int X, int Y, int Num)
     return true;
 }
 
-bool DFS(int Pos)
-{
-    if (Pos == 81)
-    {
-        return true;
-    }
 
-    int X = Pos % 9;
-    int Y = Pos / 9;
+bool DFS(int Depth)
+{
+    if (Depth == 81)
+        return true;
+
+
+    int X = Depth % 9;
+    int Y = Depth / 9;
+
     if (Matrix[Y][X] != 0)
-    {
-        return DFS(Pos + 1);
-    }
+        return DFS(Depth + 1);
 
     for (int i = 1; i <= 9; ++i)
-    {
-
+    {        
         if (Check(X, Y, i))
         {
             Matrix[Y][X] = i;
-            if (DFS(Pos + 1))
+
+            if (DFS(Depth + 1))
                 return true;
-            Matrix[Y][X] = 0;  // 백트래킹
+
+            Matrix[Y][X] = 0;
         }
 
     }
 
     return false;
 }
-
 
 
 int main()
@@ -88,12 +81,14 @@ int main()
 
     DFS(0);
 
+
     for (int i = 0; i < 9; ++i)
     {
         for (int j = 0; j < 9; ++j)
         {
             cout << Matrix[i][j] << ' ';
         }
+
         cout << endl;
     }
 
