3ddcec2b058410c556bcefe4645e701c82a6a4ee
HeroJho <89238479+HeroJho@users.noreply.github.com>
2024-07-29 10:28:27 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 3.65 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.cpp
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.cpp
@@ -3,22 +3,28 @@
 
 using namespace std;
 
-int totwo(int n){
+int totwo(int n)
+{
     int cnt=0;
     
-    while(n!=0){
-        if(n%2==1) cnt++;
+    while(n!=0)
+    {
+        if(n%2==1) 
+            cnt++;
+        
         n/=2;
     }
     
     return cnt;
 }
 
-int solution(int n) {
+int solution(int n) 
+{
     int m=totwo(n);
     n++;
     
-    while(totwo(n)!=m){
+    while(totwo(n)!=m)
+    {
         n++;
     }
     
