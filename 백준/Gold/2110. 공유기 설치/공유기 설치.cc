aecd48a7dbfa34a2e3e6904b72ea290a0e2176da
HeroJho <89238479+HeroJho@users.noreply.github.com>
2024-08-07 14:03:32 +0900

    [Gold IV] Title: 공유기 설치, Time: 32 ms, Memory: 3688 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.cc
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.cc
@@ -1,80 +1,66 @@
 #include <iostream>
 #include <vector>
+#include <climits>
 #include <algorithm>
 
 using namespace std;
 
-int g_N, g_C;
-vector<int> g_Inputs;
 
-
-bool possible(int dist)
+int main()
 {
-	// 0번째는 무조건 설치한다
-	int cnt = 1;
-	int prev = g_Inputs[0];
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
 
-	for (int i = 1; i < g_N; i++)
-	{
-		// 거리가 dist 이상일 때만 설치할 때
-		if (g_Inputs[i] - prev >= dist)
-		{
-			cnt++;
-			prev = g_Inputs[i];
-		}
-	}
-		
-	// 같거나 더 많이 설치됐다면
-	if (cnt >= g_C)
-		return true;
-
-	// 더 작게 설치됐다면
-	return false;
-}
+    // i 거리로 설치가 다 되면 크게해서 확인
+    // 설치가 안 된다면 작게해서 확인
 
 
+    int N, C;
+    cin >> N >> C;
+    vector<int> Inputs;
+    for (int i = 0; i < N; ++i)
+    {
+        int Temp;
+        cin >> Temp;
+        Inputs.push_back(Temp);
+    }
 
-int main(void)
-{
+    sort(Inputs.begin(), Inputs.end());
 
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	cin >> g_N >> g_C;
+    int Ans = 0;
+
+    int Left = 1, Right = Inputs.back() - Inputs.front();
+    while (Left <= Right)
+    {
+        int Middle = (Left + Right) / 2;
+
+        int Cnt = 1;
+        int PreIndex = 0;
+        for (int i = 1; i < Inputs.size(); ++i)
+        {
+            if (Middle <= Inputs[i] - Inputs[PreIndex])
+            {
+                ++Cnt;
+                PreIndex = i;
+            }
+        }
+
+        // 같거나 더 설치됐다
+        if (Cnt >= C)
+        {
+            Left = Middle + 1;
+            Ans = max(Ans, Middle);
+        }
+        // 덜 설치됐다
+        else if(Cnt < C)
+        {
+            Right = Middle - 1;
+        }
+
+    }
+
+    cout << Ans;
 
 
-	for (int i = 0; i < g_N; i++)
-	{
-		int Temp; cin >> Temp;
-		g_Inputs.push_back(Temp);
-	}
-		
-	sort(g_Inputs.begin(), g_Inputs.end());
-
-	//최소거리, 최대 거리
-	int Low = 1, High = g_Inputs[g_N - 1] - g_Inputs[0];
-	int Result = 0;
-
-	while (Low <= High)
-	{ 
-		int Mid = (Low + High) / 2;
-
-		// 거리가 mid 이상일 때 설치했을 때
-		if (possible(Mid)) // C 이상 설치됐다면
-		{ 
-			// 거리를 높여서 확인한다.
-			Result = max(Result, Mid);
-			Low = Mid + 1;
-		}
-		else   // C보다 더 작게 설치됐다면
-		{
-			// 거리를 낮춰서 확인해 본다.
-			High = Mid - 1;
-		}
-			
-	}
-
-	cout << Result << "\n";
-
-	return 0;
-
+    return 0;
 }
