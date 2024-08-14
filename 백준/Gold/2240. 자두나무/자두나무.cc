1249961055a99e936c0df51158d59d01ae10f089
HeroJho <89238479+HeroJho@users.noreply.github.com>
2024-08-14 15:00:37 +0900

    [Gold V] Title: 자두나무, Time: 0 ms, Memory: 1376 KB -BaekjoonHub

--- 백준/Gold/2240. 자두나무/자두나무.cc
+++ 백준/Gold/2240. 자두나무/자두나무.cc
@@ -1,59 +1,43 @@
+#include <cstdio>
+#define MAX 1001
 
-
-#include <iostream>
-#include <algorithm>
-
-using namespace std;
+int plum[MAX][3];
 int T, W;
+int DP();
 
-int list[1001];
-int dp[31][2][1001];
-// dp[이동횟수][현재위치][시간]
+int max(int a, int b) {
+    return (a > b ? a : b);
+}
+int main(void) {
+    scanf("%d %d", &T, &W);
 
-int main()
-{
-	std::ios::sync_with_stdio(false);
-	std::cin.tie(NULL);
+    for(int i = 1; i <= T; i++) {
+        int num;
+        scanf("%d", &num);
+        plum[i][num] = 1;
+    }
 
-	// 입력
-	cin >> T >> W;
-	for (int i = 1; i <= T; i++)
-		cin >> list[i];
-
-
-	// W = 이동 가능 횟수
-	// T = 시간
-	for (int j = 0; j <= W; j++)
-	{
-		for (int i = 1; i <= T; i++)
-		{
-			if (j == 0)
-			{	//처음 시작시 자두는 1번 나무 아래 위치해있다.
-				dp[j][0][i] = dp[j][0][i - 1] + (list[i] == 1);
-			}
-			else
-			{
-				//첫 시작 이후는, 위치 움직임or안움직임, 받음or못받음을 확인하며 체크한다.
-				dp[j][0][i] = max(dp[j][0][i - 1] + (list[i] == 1), dp[j - 1][1][i - 1] + (list[i] == 1));
-				dp[j][1][i] = max(dp[j][1][i - 1] + (list[i] == 2), dp[j - 1][0][i - 1] + (list[i] == 2));
-				// dp[이동횟수][자두의 위치][흐른 시간] = 
-				// (이동하지않고 시간만 1초 흐름 + i번째 입력과 현재 위치가 같은가?), 
-				// (이동했고, 1초 흐름 + i번째 입력과 현재 위치가 같은가?) 중 큰 값
-			}
-		}
-	}
-
-
-	int ans = 0;
-	for (int i = 0; i < 2; i++)
-	{
-		for (int j = 0; j <= W; j++)
-		{
-			// 마지막에 가능한 모든 이동횟수와, 위치중 가장 큰 값을 출력한다.
-			ans = max(ans, dp[j][i][T]);
-		}
-	}
-
-	cout << ans << '\n';
+    printf("%d\n", DP());
+    return 0;
 }
 
+int DP() {
+    int result = 0; // 최종 결과
+    int count[MAX][32][3]; // 자두 개수 세는 배열
+
+    count[1][0][1] = plum[1][1];
+    count[1][1][2] = plum[1][2];
+
+    result = max(count[1][0][1], count[1][1][2]);
+
+    for(int i = 2; i <= T; i++) {
+        for(int j = 0; j <= W; j++) {
+            count[i][j][1] = max(count[i - 1][j][1], count[i - 1][j - 1][2]) + plum[i][1];
+            count[i][j][2] = max(count[i - 1][j - 1][1], count[i - 1][j][2]) + plum[i][2];
+
+            result = max(max(result, count[i][j][1]), count[i][j][2]);
+        }
+    }
+
+    return result;
+}
