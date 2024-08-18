adfcb080d340104885cbb8da170c44545f57a80d
HeroJho <89238479+HeroJho@users.noreply.github.com>
2024-08-18 12:01:53 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 700 ms, Memory: 57116 KB -BaekjoonHub

--- 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.cc
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.cc
@@ -1,96 +1,96 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <climits>
+#include <algorithm>
 
 using namespace std;
-
-struct Data
-{
-    int Start, End;
-    int Dis;
-
-    Data() : Start(0), End(0), Dis(0) {};
-    Data(int start, int end, int dis) : Start(start), End(end), Dis(dis) {};
-
-};
 
 class Func
 {
 public:
-    bool operator()(Data L, Data R)
-    {
-        return L.Dis > R.Dis;
-    }
+	bool operator()(pair<int, int> L, pair<int, int> R)
+	{
+		return L.first > R.first;
+	}
 };
 
 int N, M;
+
 vector<pair<int, int>> Matrix[100001];
-vector<pair<int, int>> SPMatrix[100001];
-bool Added[100001] = { false };
+int DP[100001] = { INT_MAX };
+
+void Reset()
+{
+	for (int i = 0; i < 100001; ++i)
+	{
+		DP[i] = INT_MAX;
+	}
+}
+
+
+void Dij()
+{
+	vector<int> Lines;
+	Reset();
+
+	priority_queue<pair<int, int>, vector<pair<int, int>>, Func> Qs;
+	Qs.push({ 0,1 });
+
+	while (!Qs.empty())
+	{
+		int CurIndex = Qs.top().second;
+		int CurValue = Qs.top().first;
+		Qs.pop();
+
+		
+		if (DP[CurIndex] != INT_MAX)
+			continue;
+		DP[CurIndex] = 0;
+
+		Lines.push_back(CurValue);
+
+
+		for (int i = 0; i < Matrix[CurIndex].size(); ++i)
+		{
+			int NexIndex = Matrix[CurIndex][i].second;
+			int NexValue = Matrix[CurIndex][i].first;
+
+			Qs.push({ NexValue, NexIndex });
+		}
+
+	}
+
+	sort(Lines.rbegin(), Lines.rend());
+
+	int Ans = 0;
+	for (int i = 1; i < Lines.size(); ++i)
+	{
+		Ans += Lines[i];
+	}
+
+	cout << Ans;
+
+}
 
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-
-    cin >> N >> M;
-    for (int i = 0; i < M; ++i)
-    {
-        int Start, End, V;
-        cin >> Start >> End >> V;
-        Matrix[Start].push_back({ End, V });
-        Matrix[End].push_back({ Start, V });
-    }
+	ios::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
 
 
-    // 시작 노드 추가한다
-    // 간선을 우선순위 큐에 넣는다
-    
-    // 짧은 간선을 꺼낸다
-    // 연결된 노드를 추가한다
+	cin >> N >> M;
 
-    priority_queue<Data, vector<Data>, Func> Qs;
-    for (int i = 0; i < Matrix[1].size(); ++i)
-    {
-        Qs.push({ 1,Matrix[1][i].first, Matrix[1][i].second });
-    }
-    Added[1] = true;
+	for (int i = 0; i < M; ++i)
+	{
+		int Start, End, Value;
+		cin >> Start >> End >> Value;
+		Matrix[Start].push_back({ Value, End });
+		Matrix[End].push_back({ Value, Start });
+	}
 
-    int TotalDis = 0;
-    int MaxDis = 0;
-    while (!Qs.empty())
-    {
-        // 제일 작은 간선을 꺼낸다
-        Data Top = Qs.top();
-        Qs.pop();
+	Dij();
 
-        if (Added[Top.End])
-            continue;
-        
-        SPMatrix[Top.Start].push_back({ Top.End, Top.Dis });
-        SPMatrix[Top.End].push_back({ Top.Start, Top.Dis });
-
-        if (MaxDis < Top.Dis)
-            MaxDis = Top.Dis;
-        TotalDis += Top.Dis;
-
-        Added[Top.End] = true;
-
-        // End의 간선을 넣는다
-        for (int i = 0; i < Matrix[Top.End].size(); ++i)
-        {
-            if (Added[Matrix[Top.End][i].first])
-                continue;
-
-            Qs.push({ Top.End, Matrix[Top.End][i].first, Matrix[Top.End][i].second });
-        }
-
-    }
-
-    // 제일 긴 간선을 하나 뺀다
-    TotalDis -= MaxDis;
-    cout << TotalDis;
-
-    return 0;
+	return 0;
 }
