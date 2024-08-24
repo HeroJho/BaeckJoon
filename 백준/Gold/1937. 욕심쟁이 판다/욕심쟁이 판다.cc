734c1d75f4ca1590d5e0224dcfd9493745c38b00
HeroJho <89238479+HeroJho@users.noreply.github.com>
2024-08-24 21:17:40 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 32 ms, Memory: 4012 KB -BaekjoonHub

--- 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.cc
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.cc
@@ -1,76 +1,62 @@
-#include <iostream>
 #include <vector>
-#include <climits>
-#include <cmath>
+#include <algorithm>
+#include <iostream>
 
 using namespace std;
 
-int N;
-int Matrix[501][501] = { 0 };
-// 이 이후로 N개가 최대다
-int DP[501][501] = { 0 };
+int n, ans;
+int num[501][501];
+int dp[501][501];
 
-int DirX[] = { 0, 0, -1, 1 };
-int DirY[] = { -1, 1, 0, 0 };
+int dx[4] = { 1, 0, -1, 0 };
+int dy[4] = { 0, -1, 0, 1 };
 
-
-bool IsIn(int X, int Y)
+int dfs(int x, int y) 
 {
-    return X >= 0 && X < N&& Y >= 0 && Y < N;
+	if (dp[x][y]) 
+		return dp[x][y];
+
+	dp[x][y] = 1;
+
+	for (int i = 0; i < 4; i++) 
+	{
+		int nx = x + dx[i];
+		int ny = y + dy[i];
+
+		if (nx < 1 || nx > n || ny < 1 || ny > n)
+			continue;
+		if (num[x][y] < num[nx][ny]) 
+		{
+			dp[x][y] = max(dp[x][y], dfs(nx, ny) + 1);
+		}
+	}
+
+	return dp[x][y];
 }
 
-int DFS(int Depth, pair<int, int> Pos)
+int main() 
 {
-    if (DP[Pos.second][Pos.first] != 0)
-    {
-        return DP[Pos.second][Pos.first];
-    }
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
 
-    for (int i = 0; i < 4; ++i)
-    {
-        int nX = Pos.first + DirX[i];
-        int nY = Pos.second + DirY[i];
+	cin >> n;
 
-        if (!IsIn(nX, nY))
-            continue;
+	for (int i = 1; i <= n; i++) 
+	{
+		for (int j = 1; j <= n; j++) 
+		{
+			cin >> num[i][j];
+		}
+	}
+	for (int i = 1; i <= n; i++) 
+	{
+		for (int j = 1; j <= n; j++) 
+		{
+			ans = max(ans, dfs(i, j));
+		}
+	}
 
-        if (Matrix[Pos.second][Pos.first] < Matrix[nY][nX])
-        {
-            DP[Pos.second][Pos.first] = max(DP[Pos.second][Pos.first] , DFS(Depth + 1, { nX, nY }) + 1);
-        }
-            
+	cout << ans;
 
-    }
-
-    return DP[Pos.second][Pos.first];
-}
-
-
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-
-    cin >> N;
-    for (int i = 0; i < N; ++i)
-    {
-        for (int j = 0; j < N; ++j)
-        {
-            cin >> Matrix[i][j];
-        }
-    }
-
-    int Ans = 0;
-
-    for (int i = 0; i < N; ++i)
-    {
-        for (int j = 0; j < N; ++j)
-        {
-            Ans = max(Ans, DFS(0, { j,i }));
-        }
-    }
-
-    cout << Ans + 1;
-
-    return 0;
+	return 0;
 }
