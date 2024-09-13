718e61e4cf297ed8e527db01cc2c2dbbf83ddd0d
HeroJho <89238479+HeroJho@users.noreply.github.com>
2024-09-13 12:26:26 +0900

    [Gold V] Title: 입국심사, Time: 48 ms, Memory: 2916 KB -BaekjoonHub

--- 백준/Gold/3079. 입국심사/입국심사.cc
+++ 백준/Gold/3079. 입국심사/입국심사.cc
@@ -1,9 +1,13 @@
 #include <iostream>
-#include <algorithm>
-#include <string>
 #include <vector>
+#include <climits>
+#include <algorithm>
+#include <queue>
 
 using namespace std;
+
+int N, M;
+vector<int> Inputs;
 
 
 int main()
@@ -11,56 +15,42 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
 
-    int TimeTest[100000];
-    for(int i = 0; i < 100000; ++i)
-    {
-        TimeTest[i] = i;
-    }
-    
-
-    int N, M;
     cin >> N >> M;
-    vector<int> Times;
+    long long Max = 0;
     for (int i = 0; i < N; ++i)
     {
         int Temp;
         cin >> Temp;
-        Times.push_back(Temp);
+        Inputs.push_back(Temp);
+
+        if (Max < Temp)
+            Max = Temp;
     }
 
-  
-    sort(Times.begin(), Times.end());
 
     long long Ans = 0;
 
-    long long Left = Times[0];
-    long long Right = M * (long long)Times.back();
-
+    long long Left = 1, Right = Max * M;
     while (Left <= Right)
     {
-        long long Mid = (Right + Left) / 2;
-        long long TempM = 0;
+        long long Mid = (Left + Right) / 2;
 
-        // 현재 시간 기준으로 심사위원들이 몇명을 처리하는지 확인
-        for (int i = 0; i < Times.size(); ++i)
+        long long Cnt = 0;
+        for (int i = 0; i < Inputs.size(); ++i)
         {
-            TempM += (Mid / (long long)Times[i]);
-
-            // 오버플로 방지
-            if (M < TempM)
+            Cnt += Mid / Inputs[i];
+            if (Cnt > M)
                 break;
         }
 
-        // 현재 값 보다 많은 사람을 처리할 수 있을 경우
-        if (TempM >= M)
+        if (Cnt < M)
+        {
+            Left = Mid + 1;
+        }
+        else
         {
             Right = Mid - 1;
             Ans = Mid;
-        }
-        // 현재 값보다 적은 사람을 처리할 수 있을 경우
-        else
-        {
-            Left = Mid + 1;
         }
 
     }
