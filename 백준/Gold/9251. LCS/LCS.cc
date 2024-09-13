64d836cd42e512fd548426585564d64c93c43caa
HeroJho <89238479+HeroJho@users.noreply.github.com>
2024-09-13 13:19:53 +0900

    [Gold V] Title: LCS, Time: 4 ms, Memory: 5940 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.cc
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -4,37 +4,30 @@
 
 using namespace std;
 
-const int MAX = 1001;
 
-int dp[MAX][MAX];
+int DP[1001][1001] = {0};
 
-
-void solution(){
-
-    string a, b;
-    cin >> a >> b;
-
-    int a_size = a.length();
-    int b_size = b.length();
-
-    for (int i = 1; i <= a_size; i++)
-        for (int j = 1; j <= b_size; j++){
-            if (a[i-1] == b[j-1]) dp[i][j] = dp[i-1][j-1] + 1;
-            else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
-        }
-
-    cout << dp[a_size][b_size] << '\n';
-    
-
-
-}
-int main(){
-
+int main() 
+{
     ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
+    cin.tie(0); cout.tie(0);
 
-    solution();
+    string A, B;
+    cin >> A >> B;
+
+    for (int i = 1; i <= A.length(); i++)
+    {
+        for (int j = 1; j <= B.length(); j++)
+        {
+            if (A[i - 1] == B[j - 1]) 
+                DP[i][j] = DP[i - 1][j - 1] + 1;
+            else 
+                DP[i][j] = max(DP[i - 1][j], DP[i][j - 1]);
+        }
+    }
+        
+    cout << DP[A.length()][B.length()] << '\n';
+
 
     return 0;
 }
