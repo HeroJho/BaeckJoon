2525913aa0d0e27c31e7ca1e99b3a1fc07e7b4ea
HeroJho <89238479+HeroJho@users.noreply.github.com>
2024-09-19 14:03:55 +0900

    [Silver III] Title: 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1072. 게임/게임.cc
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    long long X, Y;
+    cin >> X >> Y;
+
+    long long Z = (Y * 100 / double(X));
+    
+    if (Z >= 99)
+    {
+        cout << -1;
+        return 0;
+    }
+        
+
+    long long Ans = 0;
+
+    long long Left = 1, Right = 1000000000;
+    while (Left <= Right)
+    {
+        long long Mid = (Left + Right) / 2;
+
+        long long nX, nY;
+        nX = X + Mid;
+        nY = Y + Mid;
+
+        long long nZ = (nY * 100 / double(nX));
+
+        // 작거나 같다면 높인다
+        if (nZ <= Z)
+        {
+            Left = Mid + 1;
+        }
+        else // 크다면 좁힌다
+        {
+            Right = Mid - 1;
+            Ans = Mid;
+        }
+
+        
+    }
+
+    cout << Ans;
+
+    return 0;
+}
