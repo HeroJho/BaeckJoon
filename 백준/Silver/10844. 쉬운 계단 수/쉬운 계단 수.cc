5f4c6b1787f5a34c25c9c6124cb2c49d18a4f0e3
HeroJho <89238479+HeroJho@users.noreply.github.com>
2024-10-15 13:26:10 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.cc
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.cc
@@ -1,9 +1,4 @@
 #include <iostream>
-#include <vector>
-#include <queue>
-#include <algorithm>
-#include <set>
-#include "limits.h"
 
 using namespace std;
 
@@ -11,57 +6,36 @@ long long DP[101][10] = { 0 };
 
 int main()
 {
-	ios::sync_with_stdio(false);
-	cin.tie(0); cout.tie(0);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
 
-	int N;
-	cin >> N;
+    int N;
+    cin >> N;
 
-	if (N == 1)
-	{
-		cout << 9;
-		return 0;
-	}
+    // DP[N][5] = DP[N-1][4] + DP[N-1][3]
 
-	// N번째 줄이 0~9일 때 경우의 수는 1;
-	for (int i = 0; i < 10; ++i)
-	{
-		DP[N][i] = 1;
-	}
+    DP[1][0] = 0;
+    for (int i = 1; i <= 9; ++i)
+        DP[1][i] = 1;
 
+    for (int n = 2; n <= N; ++n)
+    {
+        for (int i = 0; i <= 9; ++i)
+        {
+            if(i + 1 <= 9)
+                DP[n][i] = (DP[n][i] + DP[n - 1][i + 1]) % 1000000000;
+            if(i - 1 >= 0)
+                DP[n][i] = (DP[n][i] + DP[n - 1][i - 1]) % 1000000000;
 
-	for (int i = N-1; i >= 1; --i)
-	{
-		for (int j = 0; j < 10; ++j)
-		{
-			// i가 1일 때 j가 0일 경우는 제거
+        }
 
-			int PreIndex = j - 1;
-			int NexIndex = j + 1;
-			if (PreIndex < 0)
-			{
-				DP[i][j] = DP[i + 1][NexIndex];
-			}
-			else if(NexIndex > 9)
-			{
-				DP[i][j] = DP[i + 1][PreIndex];
-			}
-			else
-			{
-				DP[i][j] = DP[i + 1][PreIndex] + DP[i + 1][NexIndex];
-			}
+    }
 
-			DP[i][j] %= 1000000000;
-		}
-	}
+    long long Ans = 0;
+    for (int i = 0; i <= 9; ++i)
+        Ans =(Ans + DP[N][i]) % 1000000000;
 
-	// 나누고 더하나, 더하고 나누나 같다
+    cout << Ans;
 
-	long long Ans = 0;
-	for (int i = 1; i < 10; ++i)
-		Ans = (Ans + DP[1][i]) % 1000000000;
-
-	cout << Ans;
-
-	return 0;
+    return 0;
 }
