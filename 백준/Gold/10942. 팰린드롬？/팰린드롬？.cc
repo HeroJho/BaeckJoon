c067d1ab200f1a9168f8a763bfd63bb3f8cbd08d
HeroJho <89238479+HeroJho@users.noreply.github.com>
2024-10-24 10:37:40 +0900

    [Gold IV] Title: 팰린드롬?, Time: 224 ms, Memory: 18356 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.cc
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.cc
@@ -1,59 +1,76 @@
 #include <iostream>
 #include <vector>
-#include <queue>
 
 using namespace std;
 
+int N;
+int Inputs[2001] = { 0 };
 
 bool DP[2001][2001] = { false };
 
+
+bool IsIn(int X, int Y)
+{
+    return X >= 0 && X < N&& Y >= 0 && Y < N;
+}
+
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
 
-    int N;
     cin >> N;
-    vector<int> Inputs;
+
     for (int i = 0; i < N; ++i)
     {
-        int Temp; cin >> Temp;
-        Inputs.push_back(Temp);
+        cin >> Inputs[i];
     }
+
     int M;
     cin >> M;
-    vector<pair<int ,int>> Orders;
+    vector<pair<int, int>> Orders;
     for (int i = 0; i < M; ++i)
     {
-        pair<int, int> Temp;
-        cin >> Temp.first >> Temp.second;
-        Orders.push_back(Temp);
+        int S, E;
+        cin >> S >> E;
+        Orders.push_back({ S-1, E-1 });
     }
 
-    
-
-    for (int i = 0; i < N-1; ++i)
+    //  4 1
+    // 0 / 3
+    // DP[n][i] = DP[n-2][i] && Inputs[i-(n/2)] == Inputs[i+(n/2)]  홀수
+    // DP[n][i] = DP[n-2][i] && Inputs[i-(n/2)] == Inputs[i+(n/2) + 1]  짝수
+    for (int i = 0; i < N; ++i)
     {
-        // 하나는 무조건 true
-        DP[i][i] = true;
-        // 둘은 두 개가 같으면 true
-        DP[i][i + 1] = Inputs[i] == Inputs[i + 1];
+        DP[0][i] = true;
+        DP[1][i] = true;
     }
-    DP[N-1][N-1] = true;
 
-    // i = 간격
-    for (int i = 2; i < N; ++i)
+
+    for (int n = 2; n <= N; ++n)
     {
-        for (int j = 0; j+i < N; ++j)
+        for (int i = 0; i < N; ++i)
         {
-            DP[j][j+i] = Inputs[j] == Inputs[j+i] && DP[j + 1][j+i - 1];
+            int Left, Right;
+            Left = i - (n / 2);
+            Right = i + (n / 2);
+            if (n % 2 == 0)
+                Left += 1;
+
+            if (!IsIn(Left, Right))
+                continue;
+
+            DP[n][i] = DP[n - 2][i] && (Inputs[Left] == Inputs[Right]);
+            
         }
     }
 
-
     for (int i = 0; i < M; ++i)
     {
-        cout << DP[Orders[i].first - 1][Orders[i].second - 1] << '\n';
+        int Dis = Orders[i].second - Orders[i].first + 1;
+        int Middle = (Orders[i].second + Orders[i].first) / 2;
+
+        cout << DP[Dis][Middle] << '\n';
     }
 
 
